5ca67dd42d18f017dcfcfb800e690eb255a03860
ryanvanoss <ryanvanoss@users.noreply.github.com>
2017-11-17 01:32:44 -0800

    ci(travis): Add defaults for environment variables in sh/ci/*.sh scripts. (#24)

--- sh/ci/build.sh
+++ sh/ci/build.sh
@@ -9,7 +9,12 @@ set -ev
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DOCKER_UTIL_PATH=$DIR/../../docker-util
 
+: ${ALPINE_VERSION:=latest}
+: ${DOCKER_IMAGE_NAME:=travis-cli}
+: ${IMAGE_SIZE_THRESHOLD:=35}
 
-
+# build the image
 docker build --build-arg ALPINE_VERSION=$ALPINE_VERSION --tag $DOCKER_IMAGE_NAME --compress --force-rm --squash .
+
+# test the image size
 $DOCKER_UTIL_PATH/sh/test-image-size.sh -i $DOCKER_IMAGE_NAME -t $IMAGE_SIZE_THRESHOLD
--- sh/ci/test-deploy.sh
+++ sh/ci/test-deploy.sh
@@ -5,10 +5,15 @@
 # @see https://docs.travis-ci.com/user/customizing-the-build/#Implementing-Complex-Build-Steps
 set -ev
 
+: ${DOCKER_PUBLIC_IMAGE_NAME:=beeglercorpstage/travis-cli}
+: ${TAG:=latest}
 IMAGE_WITH_TAG=$DOCKER_PUBLIC_IMAGE_NAME:$TAG
 
-
-
+# pull an existing image from the Docker registry
 docker pull $IMAGE_WITH_TAG
+
+# output the Travis CLI version
 docker run --rm $IMAGE_WITH_TAG --version
+
+# run `travis lint` against this repo's .travis.yml file
 docker run --mount type=bind,source=$(pwd)/.travis.yml,target=/.travis.yml --rm $IMAGE_WITH_TAG lint /.travis.yml
